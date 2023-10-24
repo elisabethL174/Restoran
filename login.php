c180f44123c9d63d74d40a807977682187b61c0c
arzavitohidayat <arza.vito@student.umn.ac.id>
2023-10-24 18:37:04 +0700

    typo el munchero

--- login.php
+++ login.php
@@ -121,7 +121,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php">El Muchero</a>
+        <a class="navbar-brand" href="index.php">El Munchero</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
